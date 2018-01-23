422bac70efe90c26b23491cdb9b89949b1bff4fd
ibrahimessa1 <35236104+ibrahimessa1@users.noreply.github.com>
2018-01-23 20:58:24 +0500

    Add files via upload

---
+++ Module1.vb
@@ -0,0 +1,29 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim MemberName As String = ""
+        Dim MemberId As Integer = 0
+
+        FileOpen(1, "D:\VB example\members.txt", OpenMode.Append)
+
+        Console.WriteLine("Enter Members name:   ")
+        MemberName = Console.ReadLine
+
+        Console.WriteLine("Enter Members Id:  ")
+        MemberId = Console.ReadLine
+
+        WriteLine(1, MemberName)
+        WriteLine(1, MemberId)
+
+        Console.WriteLine(MemberName & MemberId)
+
+
+
+        FileClose(1)
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
