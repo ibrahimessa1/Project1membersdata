02c3e5f1a29b2e091d8d05cd277cf968b78b1c22
ibrahimessa1 <35236104+ibrahimessa1@users.noreply.github.com>
2018-01-28 15:58:21 +0500

    Add files via upload

--- Module1.vb
+++ Module1.vb
@@ -1,28 +1,38 @@
 ﻿Module Module1
 
     Sub Main()
+
+        Dim SearchName As String = ""
         Dim MemberName As String = ""
         Dim MemberId As Integer = 0
+        Dim IsFound As Boolean = False
 
-        FileOpen(1, "D:\VB example\StudentRecord.txt", OpenMode.Input)
+        FileOpen(1, "D:\VB example\Members.txt", OpenMode.Input)
 
-        While Not EOF(1)
+        Console.WriteLine("Enter the name you want to search for:  ")
+        SearchName = Console.ReadLine
+
+        While Not EOF(1) And IsFound = False
+
             Input(1, MemberName)
-
             Input(1, MemberId)
 
-            Console.WriteLine("Member Name : " & MemberName)
-            Console.WriteLine("Member ID : " & MemberId)
+            If SearchName = MemberName Then
+                IsFound = True
+                Console.WriteLine("Member Name : " & MemberName)
+                Console.WriteLine("Member ID : " & MemberId)
+
+            End If
 
         End While
+
+        If IsFound = False Then
+            Console.WriteLine("Record Not Found...!")
+        End If
 
         FileClose(1)
 
         Console.ReadKey()
-
-
-
-
 
     End Sub
 
